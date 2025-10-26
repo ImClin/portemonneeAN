9a48424f67d9a66bad20f78f5d6afaedf767919d
Colin Canters <colincanters@gmail.com>
2025-10-26 17:59:36 +0100

    Initialize project structure with Gradle, including .gitignore, build configuration, and main plugin files.

---
+++ src/main/java/me/clin/portemonneeAN/PortemonneeAN.java
@@ -0,0 +1,17 @@
+package me.clin.portemonneeAN;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PortemonneeAN extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
